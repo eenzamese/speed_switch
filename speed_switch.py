0fce6fc789620fd07148d86aa3dd3c7f95b03760
eenzamese <eenzamese@gmail.com>
2024-11-15 10:23:07 +0000

    gitignore

--- speed_switch.py
+++ speed_switch.py
@@ -1,35 +1,40 @@
 """Gates filter"""
-import socket
+import pathlib
 import time
 import logging
 import re
 import sys
 import sqlite3
-from os import listdir, sep
-from os.path import dirname, basename
+import pdb
+from os import sep
+from os.path import dirname
 import speedtest
 from statistics import mean
-from datetime import date, datetime, time, timedelta
+import datetime as dt
+from datetime import timedelta
 
 
 # constants
 APP_TMT = 60
 LOG_START_TIME = re.sub(r"\W+", "_", str(time.ctime()))
 
+LOG_FMT_STRING = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 
 if getattr(sys, 'frozen', False):
     app_path = dirname(sys.executable)
-    app_name = basename(sys.executable)
+    app_name = pathlib.Path(sys.executable).stem
     APP_RUNMODE = 'PROD'
     time.sleep(APP_TMT)
 else:
     app_path = dirname(__file__)
-    app_name = basename(__file__)
+    app_name = pathlib.Path(__file__).stem
     APP_RUNMODE = 'TEST'
 
-
-LOG_FMT_STRING = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 LOG_FILENAME = f'{app_path}{sep}{app_name}_{LOG_START_TIME}.log'
+log_handlers = [logging.StreamHandler()]
+
+if APP_RUNMODE == 'PROD':
+    log_handlers = log_handlers.append(logging.FileHandler(LOG_FILENAME))
 
 logger = logging.getLogger(APP_RUNMODE)
 logging.basicConfig(format=LOG_FMT_STRING,
@@ -37,7 +42,7 @@ logging.basicConfig(format=LOG_FMT_STRING,
                     level=logging.INFO, # NOTSET/DEBUG/INFO/WARNING/ERROR/CRITICAL
                     handlers=[logging.FileHandler(LOG_FILENAME),
                               logging.StreamHandler()])
-sys.exit()
+
 
 def tb_init(in_table_name, in_conn=None, in_c=None):
     """Get table initialization"""
@@ -56,8 +61,7 @@ def tb_init(in_table_name, in_conn=None, in_c=None):
     return result
 
 
-
-DB_NAME = f'{app_path}{sep}db_online.sqlite'
+DB_NAME = f'{app_path}{sep}{app_name}_db.sqlite'
 TB_NAME = 'measures'
 
 
@@ -73,6 +77,7 @@ except Exception as ex: # pylint: disable=broad-exception-caught
     str_out = 'DB connection is FAIL'# pylint: disable=invalid-name
     logger.critical(str_out)
     sys.exit()
+
 
 tb_init_result = tb_init(TB_NAME, in_conn = conn, in_c = c)
 if not tb_init_result['result']:
@@ -90,50 +95,27 @@ while True:
         s.download(threads=threads)
         s.upload(threads=threads)
         s.results.share()
-        cur_mesure = s.results.dict()['download']
+        cur_measure = s.results.dict()['download']
         with conn:
             statement = f"insert into '{TB_NAME}' \
-                        values('{str(time.ctime())}', \
-                        '{str(1)}');"
+                        values('{dt.datetime.now().date()}', \
+                        '{cur_measure}');"
             c.execute(statement)
-        date_start = datetime.combine(date.toda(), time())
-        date_end = datetime.combine(date.toda(), time()) + timedelta(days=5)
+        date_start_db = dt.datetime.now().date() - timedelta(days=1)
+        date_end_db = dt.datetime.now().date() - timedelta(days=5)
         with conn:
-            cd_statement = (f'select measure from "{TB_NAME}" '
-                         'where date between "{date_start}" and "{date_end}"')
+            cd_statement = f'select speed from "{TB_NAME}" where date between date("{date_start_db}") and date("{date_end_db}")'
+            print(cd_statement)
             measures = c.execute(cd_statement).fetchall()
-            m_measures = mean(measures)
+            m_measures = mean([el[0] for el in measures])
         if cur_measure*10 < m_measures:
-            BLOCK_ICMP_CMD = 'netsh advfirewall firewall add rule \
-                              name="ICMP ALLOW" protocol=icmpv4:8,any \
-                              dir=in acction=allow'
-            os.system(BLOCK_ICMP_CMD)
+            print("301")
+            continue
         else:
-            ALLOW_ICMP_CMD = 'netsh advfirewall firewall add rule \
-                            name="ICMP ALLOW" protocol=icmpv4:8,any \
-                            dir=in acction=allow'
-            os.system(ALLOW_ICMP_CMD)
+            print("200")
+            time.sleep(600)
     except Exception as ex: # pylint: disable=broad-exception-caught
         logger.warning(str(ex))
-        time.sleep(3)
+        time.sleep(10)
+        print("500")
         continue
-    for gate_address in data_r:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        result = sock.connect_ex((gate_address, 443))
-        if result == 0:
-            str_out = f'Gate address is {gate_address}'
-            logger.info(str_out)
-            servers.append(gate_address)
-        else:
-            STR_OUT = 'Gate address is FAILED'
-            logger.info(STR_OUT)
-            continue
-        sock.close()
-    with open(OUTPUT_FILE, 'w') as file: # pylint: disable=unspecified-encoding
-        DATA_W = '\n'.join(servers)
-        file.write(DATA_W)
-    STR_OUT = 'State is OK'
-    logger.info(STR_OUT)
-    time.sleep(1800)
-
-sys.exit()
